ba27a1cd4205685b1d9cd8515ce0c2a03375e183
Nakamal <nickhaugland@gmail.com>
2018-10-08 17:54:12 -0500

    terminal runner working with api for a player to select a character before error, although more data is need, app may be best in history, the story continues

--- clue_the_runner.rb
+++ clue_the_runner.rb
@@ -32,8 +32,52 @@ class MainGame
   include GamesController
 
   def initialize
-    @players = []
+    @current_game_id = HTTP.post("http://localhost:3000/api/games").parse["id"]
+
+    puts 
+    print "player 1 & 2: "
+    @player_id = gets.chomp.to_i
+    puts 
+
+    pick_player
+
   end
+
+  def pick_player
+    while true
+      puts 
+      characters_index_action
+      puts
+      print "pick a character id: "  
+      character_id = gets.chomp.to_i
+
+      parsed_respone = HTTP.post("http://localhost:3000/api/games/#{@current_game_id}/participations?character_id=#{character_id}&player_id=#{@player_id}").parse
+
+      if parsed_respone["move_forward"]
+        @player = Player.new()
+        return 
+      else
+
+      end
+    end
+  end
+
+  # def participation
+  #   puts 
+  #   puts "Will you be player 1 or 2 today? "
+  #   selection = gets.chomp
+  #   puts
+  #   if selection == "1"
+  #     puts "which character would you like to choose? "
+  #     @players << @characters 
+  #     p @players
+  #   elsif selection == "2"
+  #     puts
+  #     puts "And who will you be?"
+  #     @players << @characters
+  #     p @players
+  #   end 
+  # end
 
   def run
     system "clear"
@@ -50,8 +94,11 @@ class MainGame
       puts "   [4] Pick where you think the murder happened"
       puts "   [5] Look at what they could have used"
       puts "   [6] What you think they commited the murder with"
+      puts "   [7] Play the game and solve...(thunder and lightning crash)...a murder?"
       puts
-      puts "   [7] Make your accusation"
+      puts "   [8] Make a suggestion"
+      puts
+      puts "   [0] Make your accusation"
       puts
       puts "   [quit] Stop"
 
@@ -70,6 +117,8 @@ class MainGame
       elsif choice == "6"
         weapons_show_action
       elsif choice == "7"
+        participation
+      elsif choice == "0"
         accusation_action
       elsif choice == "quit"
         exit
--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -19,21 +19,3 @@ module GamesController
     accusation_view(character: characters_hash[character_id], room: rooms_hash[room_id], weapon: weapons_hash[weapon_id])
   end
 end
-
-# module GamesController
-#   def accusation_action    
-#     accusation_view(character: accusation_helper("character"), weapon: accusation_helper("weapon"), room: accusation_helper("room"))
-#   end
-
-#   def accusation_helper(type)
-#     code_string = <<-PICK
-#     action_result = #{type}s_index_action
-#     print "Which #{type} id? " 
-#     type_id = gets.chomp.to_i
-#     types_in_hash = action_result.map {|type_hash| [type_hash["id"], type_hash["name"]] }.to_h
-#     types_in_hash[type_id]
-#     PICK
-
-#     eval(code_string)
-#   end
-# end
